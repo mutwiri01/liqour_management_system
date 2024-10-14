92f0c029e150ef1189253f637f6625c80d4b3627
mgthedev <mutwiritwish00@gmail.com>
2024-10-14 15:14:04 +0300

    eee

--- server/index.js
+++ server/index.js
@@ -48,7 +48,7 @@ app.patch("/api/liquors/:id", async (req, res) => {
 // GET route to fetch sales
 app.get("/api/sales", async (req, res) => {
   try {
-    const sales = await Sale.find().populate("liquor").limit(10).lean(); // Populate liquor details
+    const sales = Sale.find().populate("liquor").limit(10).lean();// Populate liquor details
     res.status(200).json(sales);
   } catch (error) {
     console.error("Error fetching sales:", error);
@@ -61,6 +61,7 @@ app.get("/api/sales", async (req, res) => {
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Resource API 83' });
 });
+
 
 // POST route to record a sale
 app.post("/api/sales", async (req, res) => {
@@ -101,12 +102,9 @@ app.post("/api/sales", async (req, res) => {
 if (process.env.NODE_ENV !== "production") {
   connectDB()
     .then(() => {
-      const server = app.listen(port, () => {
+      app.listen(port, () => {
         console.log(`Server running on port ${port}`);
       });
-      
-      // Set a timeout of 60 seconds
-      server.setTimeout(60000);
     })
     .catch((error) => {
       console.log("Failed to connect to MongoDB. Server not started.");
